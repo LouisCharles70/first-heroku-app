8383efd823560eaaf271af38440da029c76928f5
LouisCharles70 <54309034+LouisCharles70@users.noreply.github.com>
2021-07-16 10:55:00 +0800

    Prevent CORS errors

--- index.js
+++ index.js
@@ -2,6 +2,14 @@ const startupDebugger = require('debug')('app:startup');
 
 const express = require('express');
 const app = express();
+
+app.use(function(req, res, next) {
+   res.header("Access-Control-Allow-Origin", "*"); // update to match the domain you will make the request from
+   res.header("Access-Control-Allow-Headers", "*");
+   res.header("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PUT, DELETE");
+   next();
+});
+
 require('./startup/routes')(app);
 const winston = require('winston');
 
@@ -17,7 +25,7 @@ if(app.get('env')==='development'){
    startupDebugger("Morgan enabled");
 }
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 3900;
 const server = app.listen(port,() => {
    winston.info(`Listening on ${port}`);
 })
